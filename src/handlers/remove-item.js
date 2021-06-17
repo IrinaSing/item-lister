7d8e07534615618b564e4c4c989bea264236a6fd
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-17 21:53:30 +0200

    handler remove

---
+++ src/handlers/remove-item.js
@@ -0,0 +1,8 @@
+export const removeItem = (e) => {
+    if(e.target.classList.contains('delete')) {
+      if(confirm('Are you sure?')){
+        const li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
+    }
+  }
