8f298ae1d31f81c4a51696c6556582a436efec98
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-17 22:04:57 +0200

    handler filter

---
+++ src/handlers/filter-items.js
@@ -0,0 +1,17 @@
+const itemList = document.getElementById('items');
+
+export const filterItems = (e) => {
+  // convert text to lowercase
+  const text = e.target.value.toLowerCase();
+  // get lis (html collection, not array)
+  const items = itemList.getElementsByTagName('li');
+  // convert to array
+  Array.from(items).forEach(function(item) {
+    const itemName = item.firstChild.textContent;
+    if(itemName.toLocaleLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
