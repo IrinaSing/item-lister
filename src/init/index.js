21124fc5d6e4e6c162f574159c172f0c17bb2b83
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-17 21:24:06 +0200

    var to const

--- src/init/index.js
+++ src/init/index.js
@@ -1,6 +1,6 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+const form = document.getElementById('addForm');
+const itemList = document.getElementById('items');
+const filter = document.getElementById('filter');
 
 //form submit event
 form.addEventListener('submit', addItem);
@@ -14,17 +14,17 @@ function addItem(e) {
   e.preventDefault();
 
   // get input value
-  var newItem = document.getElementById('item').value;
+  const newItem = document.getElementById('item').value;
 
   // create new li element
-  var li = document.createElement('li');
+  const li = document.createElement('li');
   // add class
   li.className = 'list-group-item';
   // add text node with input value
   li.appendChild(document.createTextNode(newItem));
 
   // create delete button element
-  var deleteBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
 
   // add classes to delete btn
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
@@ -42,7 +42,7 @@ function addItem(e) {
 function removeItem(e) {
   if(e.target.classList.contains('delete')) {
     if(confirm('Are you sure?')){
-      var li = e.target.parentElement;
+      const li = e.target.parentElement;
       itemList.removeChild(li);
     }
   }
@@ -51,12 +51,12 @@ function removeItem(e) {
 
 function filterItems(e) {
   // convert text to lowercase
-  var text = e.target.value.toLowerCase();
+  const text = e.target.value.toLowerCase();
   // get lis (html collection, not array)
-  var items = itemList.getElementsByTagName('li');
+  const items = itemList.getElementsByTagName('li');
   // convert to array
   Array.from(items).forEach(function(item) {
-    var itemName = item.firstChild.textContent;
+    const itemName = item.firstChild.textContent;
     if(itemName.toLocaleLowerCase().indexOf(text) != -1) {
       item.style.display = 'block';
     } else {
